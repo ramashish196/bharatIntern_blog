ce5c7330d494b786ab526b03394c86f6e7ef4b3a
Ramashish <ramashishvishwakarma1@gmail.com>
2024-07-10 22:26:35 +0530

    commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,36 @@
+export default function About() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About MyBlog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to Ramashish Vishwakarma's Blog! This blog was created by
+              Ramashish Vishwakarma as a personal project to share his thoughts
+              and ideas with the world. I'm a passionate MERN developer who
+              loves to write about technology, coding, and everything in
+              between.
+            </p>
+
+            <p>
+              On this blog, you'll find articles and tutorials on topics such as
+              web development, software engineering, and programming languages.
+              I'm always learning and exploring new technologies, so be sure to
+              check back often for new content!
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
